c66ffc904538199109a492651e43b468a1e7caf5
rachelofyork <rachelofyork@gmail.com>
2022-05-08 15:05:39 +0100

    Key.js now uses Enter key to move to next attempt

--- src/components/Key.js
+++ src/components/Key.js
@@ -5,6 +5,10 @@ export default function Key({keyVal, bigKey}){
      const {guesses, setGuesses, currentAttempt, setCurrentAttempt} = useContext(AppContext); /*need to import the current board state*/
 
     const selectLetter = () => {
+        if (keyVal === "Enter"){
+            if (currentAttempt.letterPos !== 5) return; /*if letter attempt is not equal to 5, end function*/
+            setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0});
+        } else {
         if (currentAttempt > 4) return; /*stops after 5 letters are typed*/
         const newBoard = [...guesses] 
         newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal /*first[0] is for attempt number, second[0] is for letter number 
@@ -13,6 +17,7 @@ export default function Key({keyVal, bigKey}){
         setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos + 1});
         /*above state equal to what it was before, and moves letter position on by one*/
     }
+};
     return <div className="key" id={bigKey && "big"} onClick={selectLetter}>{keyVal}</div>
     /*if a key is a bigKey it will be assigned the id of #big, otherwise keys will no no id*/
 }
