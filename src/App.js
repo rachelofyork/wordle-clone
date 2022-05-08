2ee4fcd9624b0c214a9d389f29df48e095996210
rachelofyork <rachelofyork@gmail.com>
2022-05-08 16:12:14 +0100

    Moved functions into App.js from Key.js
    Moved onSelectLetter, onEnter, and onDelete into App.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,36 @@ function App() {
   const [guesses, setGuesses] = useState(guessesDefault) /*pulls in matrix from Words.js and sets it as the default state for Guesses*/
   const [currentAttempt, setCurrentAttempt] = useState({attempt: 0, letterPos: 0}) 
   /* above state keeps track of attempt and letter position you're on*/
+  
+  const onSelectLetter = (keyVal) => {
+      if (currentAttempt > 4) return; /*stops after 5 letters are typed*/
+        const newBoard = [...guesses] 
+        newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal /*first[0] is for attempt number, second[0] is for letter number 
+        Here changing letter square to be equal to keyVal*/
+        setGuesses(newBoard);
+        setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos + 1});
+        /*above state equal to what it was before, and moves letter position on by one*/
+  }
+
+  const onEnter = () => {
+    if (currentAttempt.letterPos !== 5) return; /*if letter attempt is not equal to 5, end function*/
+    setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0}); /*moves to next line, first(0)square*/
+  }
+
+  const onDelete = () => {
+ if (currentAttempt.letterPos === 0) return; /*can't delete anything if it's on first letter*/
+            const newBoard = [...guesses] 
+        newBoard[currentAttempt.attempt][currentAttempt.letterPos -1] = ""; /*goes back 1 and sets square to empty string*/
+        setGuesses(newBoard)
+        setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos - 1});
+  }
+  
+
+  
   return (
     <div className="App">
       <nav><h1>Wordle</h1></nav>
-      <AppContext.Provider value={{guesses, setGuesses, currentAttempt, setCurrentAttempt}}> 
+      <AppContext.Provider value={{guesses, setGuesses, currentAttempt, setCurrentAttempt, onSelectLetter, onEnter, onDelete}}> 
       <div className='wrapper'>
         <Guesses />
       <Keyboard />
--- src/components/Key.js
+++ src/components/Key.js
@@ -2,27 +2,16 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 export default function Key({keyVal, bigKey}){
-     const {guesses, setGuesses, currentAttempt, setCurrentAttempt} = useContext(AppContext); /*need to import the current board state*/
+     const {guesses, setGuesses, currentAttempt, setCurrentAttempt, onSelectLetter, onEnter, onDelete} = useContext(AppContext); /*need to import the current board state*/
 
     const selectLetter = () => {
         if (keyVal === "Enter"){
-            if (currentAttempt.letterPos !== 5) return; /*if letter attempt is not equal to 5, end function*/
-            setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0}); /*moves to next line, first(0)square*/
+           onEnter()
         } else if (keyVal === "Delete"){
-            if (currentAttempt.letterPos === 0) return; /*can't delete anything if it's on first letter*/
-            const newBoard = [...guesses] 
-        newBoard[currentAttempt.attempt][currentAttempt.letterPos -1] = ""; /*goes back 1 and sets square to empty string*/
-        setGuesses(newBoard)
-        setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos - 1});
+           onDelete()
         }
          else {
-        if (currentAttempt > 4) return; /*stops after 5 letters are typed*/
-        const newBoard = [...guesses] 
-        newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal /*first[0] is for attempt number, second[0] is for letter number 
-        Here changing letter square to be equal to keyVal*/
-        setGuesses(newBoard);
-        setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos + 1});
-        /*above state equal to what it was before, and moves letter position on by one*/
+        onSelectLetter(keyVal)
     }
 };
     return <div className="key" id={bigKey && "big"} onClick={selectLetter}>{keyVal}</div>
