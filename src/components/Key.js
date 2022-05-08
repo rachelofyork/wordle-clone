f33c181af84ba1c3911292865d57d6b6c3a00fbe
rachelofyork <rachelofyork@gmail.com>
2022-05-08 15:31:27 +0100

    Added functionality for Delete key

--- src/components/Key.js
+++ src/components/Key.js
@@ -7,8 +7,15 @@ export default function Key({keyVal, bigKey}){
     const selectLetter = () => {
         if (keyVal === "Enter"){
             if (currentAttempt.letterPos !== 5) return; /*if letter attempt is not equal to 5, end function*/
-            setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0});
-        } else {
+            setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0}); /*moves to next line, first(0)square*/
+        } else if (keyVal === "Delete"){
+            if (currentAttempt.letterPos === 0) return; /*can't delete anything if it's on first letter*/
+            const newBoard = [...guesses] 
+        newBoard[currentAttempt.attempt][currentAttempt.letterPos -1] = ""; /*goes back 1 and sets square to empty string*/
+        setGuesses(newBoard)
+        setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos - 1});
+        }
+         else {
         if (currentAttempt > 4) return; /*stops after 5 letters are typed*/
         const newBoard = [...guesses] 
         newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal /*first[0] is for attempt number, second[0] is for letter number 
